26d0ebc5c9b6efe44014f0d849dce9f7fd4990a5
DCSItoDIGA <guido.agnesmeyer@itagnesmeyer.de>
2021-12-01 19:23:03 +0100

    Test Border

--- DigaCoreTest/BrowserWindow.cs
+++ DigaCoreTest/BrowserWindow.cs
@@ -1,6 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using CoreWindowsWrapper;
+using Diga.Core.Api.Win32;
 using Diga.NativeControls.WebBrowser;
+using Diga.WebView2.Wrapper;
 using Diga.WebView2.Wrapper.EventArguments;
 
 namespace ConsoleCaller
@@ -12,12 +15,12 @@ namespace ConsoleCaller
         {
             this.Text = "WebBrowser";
             this.Name = "BrowserWindow";
-            this.StatusBar = true;
+            this.StatusBar = false;
             this.IconFile = "Browser.ico";
             this.Width = 800;
             this.Height = 600;
             this.StartUpPosition = WindowsStartupPosition.CenterScreen;
-          
+            
             
             this._Browser = new NativeWebBrowser()
             {
@@ -25,8 +28,8 @@ namespace ConsoleCaller
                 Height = this.Height,
                 Url = "http://localhost:1",
                 IsStatusBarEnabled = true,
-                DefaultContextMenusEnabled = false,
-                DevToolsEnabled = false,
+                DefaultContextMenusEnabled = true,
+                DevToolsEnabled = true,
                 EnableMonitoring = true,
                 MonitoringFolder = ".\\wwwroot",
                 MonitoringUrl = "http://localhost:1/",
@@ -38,12 +41,46 @@ namespace ConsoleCaller
             this._Browser.NavigationCompleted += OnNaviationCompleted;
             this._Browser.WebResourceRequested += OnWebResourceRequested;
             this._Browser.WebMessageReceived += OnWebMessageReceived;
+            this._Browser.AcceleratorKeyPressed += OnWebBrowserAcceleratorKeyPressed;
+
+          
             this.Controls.Add(this._Browser);
         }
 
+        private uint LastStype = 0;
+        private uint LastExStyle = 0;
+        private void OnWebBrowserAcceleratorKeyPressed(object sender, AcceleratorKeyPressedEventArgs e)
+        {
+            if (e.VirtualKey == 122 && e.KeyVentType == KeyEventType.KeyDown)
+            {
+                IntPtr style = User32.GetWindowLongPtr(this.Handle, GWL.GWL_STYLE);
+                IntPtr exStyle = User32.GetWindowLongPtr(this.Handle, GWL.GWL_EXSTYLE);
+                this.LastStype = unchecked((uint)style.ToInt64());
+                this.LastExStyle = unchecked((uint)exStyle.ToInt64());
+                uint currentStyle = this.LastStype;
+                uint currentExStyle = this.LastExStyle;
+                currentExStyle &= WindowStylesConst.WS_EX_CLIENTEDGE;
+                currentStyle &= ~(WindowStylesConst.WS_BORDER   | WindowStylesConst.WS_SIZEBOX   |
+                                    WindowStylesConst.WS_SIZEBOX  );
+                currentStyle |= WindowStylesConst.WS_POPUP;
+                IntPtr p = new IntPtr(currentStyle);
+                IntPtr pp = new IntPtr(currentExStyle);
+                User32.SetWindowLongPtr(this.Handle, GWL.GWL_STYLE, p);
+                //User32.SetWindowLongPtr(this.Handle, GWL.GWL_EXSTYLE, pp);
+                User32.SetWindowPos(this.Handle, IntPtr.Zero, 0, 0, 0, 0,
+                    (uint)DeferWindowPosCommands.SWP_FRAMECHANGED | (uint)DeferWindowPosCommands.SWP_NOMOVE |
+                    (uint)DeferWindowPosCommands.SWP_NOSIZE | (uint)DeferWindowPosCommands.SWP_NOZORDER |
+                    (uint)DeferWindowPosCommands.SWP_NOOWNERZORDER);
+
+
+                MessageBox.Show(this.Handle, "Accelerator", "KeyPress");
+            }
+        }
+
+
         private void OnWebMessageReceived(object sender, WebMessageReceivedEventArgs e)
         {
-            
+
         }
 
         private void OnWebResourceRequested(object sender, WebResourceRequestedEventArgs e)
